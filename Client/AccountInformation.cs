c06482314073c1239fd995b21f1a34e5eff06733
jkwehr5 <jaydenkwehr@gmail.com>
2024-03-01 12:50:51 -0600

    created mock data for the dashboard page

---
+++ Client/AccountInformation.cs
@@ -0,0 +1,25 @@
+﻿namespace Client
+{
+    public partial class AccountInformation
+    {
+
+        public string AccountName { get; set; }
+
+        public Accounts[] Accounts { get; set; }
+
+        public int[] PastYearInvestments { get; set; }
+
+    }
+
+    public partial class Accounts
+    {
+        public string AccountType { get; set; }
+
+        public int AccountAmount { get; set; }
+
+        public int AccountNumber { get; set; }
+
+
+    }
+
+}
