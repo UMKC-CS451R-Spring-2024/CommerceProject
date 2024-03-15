05f1631fd8fa7d58ce5637742620e98020703cca
jwehr5 <jaydenkwehr@gmail.com>
2024-03-15 17:15:08 -0500

    added a search function in searching for stocks

---
+++ Client/model/StockData.cs
@@ -0,0 +1,19 @@
+﻿namespace Client
+{
+    public class StockData
+    {
+        public string Timestamp { get; set; }
+
+        public string Symbol { get; set; }
+
+        public double Open { get; set; }
+
+        public double High { get; set; }
+
+        public double Low { get; set; }
+
+        public double Close { get; set; }
+
+        public int Volume { get; set; }
+    }
+}
