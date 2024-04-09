5d1d7348e0571f75a631d7f87b8471b402fada70
jwehr5 <jaydenkwehr@gmail.com>
2024-04-09 00:01:50 -0500

    Added a footer. Implementing some dashboard functionality.

--- Client/model/AccountInformation.cs
+++ Client/model/AccountInformation.cs
@@ -1,25 +1,38 @@
-﻿namespace Client
+﻿using Newtonsoft.Json;
+
+
+namespace Client
 {
     public partial class AccountInformation
     {
-
+        [JsonProperty("accountName")]
         public string AccountName { get; set; }
 
+
+        [JsonProperty("accounts")]
         public Accounts[] Accounts { get; set; }
 
+        [JsonProperty("pastYearInvestments")]
         public int[] PastYearInvestments { get; set; }
+
+
+        [JsonProperty("financialAdvisorInfo")] 
+        public string[] FinancialAdvisorInfo { get; set; }
+
 
     }
 
     public partial class Accounts
     {
+        [JsonProperty("accountType")]
         public string AccountType { get; set; }
 
-        public int AccountAmount { get; set; }
+        [JsonProperty("accountAmount")]
+        public long AccountAmount { get; set; }
 
-        public int AccountNumber { get; set; }
-
-
+        [JsonProperty("accountNumber")]
+        public long AccountNumber { get; set; }
     }
+
 
 }
