6c51188f1562a66300084ae00fa2ea072553d57d
Smoothjedi <55415231+Smoothjedi@users.noreply.github.com>
2024-04-10 18:26:19 -0500

    Moved code blocks on the Razor pages to code behinds. Moved classes defined in code blocks to their own directory/namespace

---
+++ Client/Pages/Data/Retirement/RetirementResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Client.Pages.Data.Retirement
+{
+    public partial class Retirement
+    {
+        public class RetirementResult
+        {
+            public double RetirementValue { get; set; }
+            public double ActualMonthlyIncome { get; set; }
+            public double NeededAmount { get; set; }
+            public double NeededContribution { get; set; }
+            public List<double> ActualData { get; set; }
+            public List<double> NeededData { get; set; }
+        }
+    }
+}
