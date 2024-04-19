098b01b64f1f188b1ec3fa368aa7b6f2e492155e
Seth Grisham <seth@sethgrisham.com>
2024-04-18 22:59:02 -0500

    Add Client.Tests project with bUnit

---
+++ Client.Tests/Pages/UserSettingsTests.cs
@@ -0,0 +1,14 @@
+using Bunit;
+using Xunit;
+using Client.Pages;
+
+namespace Client.Tests.Pages {
+    public class UserSettingsTests : TestContext {
+       [Fact] 
+       public void UserSettingsLoggedOutRendersCorrectly() {
+            var cut = RenderComponent<UserSettings>();
+
+            cut.MarkupMatches("")
+       }
+    }
+}
