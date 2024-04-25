3f447e567b8b7eca97e9abac53a54aa3e7634521
Smoothjedi <55415231+Smoothjedi@users.noreply.github.com>
2024-04-25 00:40:26 -0500

    Changed stocks.razor.cs to use the stock repository

--- Client/Pages/Stocks.razor.cs
+++ Client/Pages/Stocks.razor.cs
@@ -1,3 +1,4 @@
+using API.Responses;
 using Newtonsoft.Json;
 
 namespace Client.Pages
@@ -5,11 +6,11 @@ namespace Client.Pages
     public partial class Stocks
     {
         string apiKey = "KRY4J4J7NUX3OZG8";
-        string searchQuery = "";
+        string searchQuery = string.Empty;
         string url = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=tesco&apikey=demo";
 
         //Store the search results into this object
-        private StockSearchInfo? searchResults;
+        private IEnumerable<StockMatch> searchResults;
 
         //public List<StockSearchInfo> listOfSearchResults = new List<StockSearchInfo>();
 
@@ -18,32 +19,33 @@ namespace Client.Pages
         public  async void getSearchResults()
         {
 
-            if (!searchQuery.Equals(""))
+            if (!string.IsNullOrWhiteSpace(searchQuery))
             {
                 showErrorMessage = false;
+                searchResults = (await stockRepository.GetStockMatches(searchQuery)).StockMatches;
 
                 //url = $"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={searchQuery}&apikey={apiKey}";
 
                 //Fetch the data from the AlphaVantage API
-                using (HttpClient http = new HttpClient())
-                {
-                    HttpResponseMessage response = await http.GetAsync(url);
+                //using (HttpClient http = new HttpClient())
+                //{
+                //    HttpResponseMessage response = await http.GetAsync(url);
 
-                    // Ensure the response was successful
-                    response.EnsureSuccessStatusCode();
+                //    // Ensure the response was successful
+                //    response.EnsureSuccessStatusCode();
 
-                    // Read the content of the response as a string
-                    string responseBody = await response.Content.ReadAsStringAsync();
+                //    // Read the content of the response as a string
+                //    string responseBody = await response.Content.ReadAsStringAsync();
 
-                    // Print the JSON data
-                    //Console.WriteLine(responseBody);
+                //    // Print the JSON data
+                //    //Console.WriteLine(responseBody);
 
-                    searchResults = JsonConvert.DeserializeObject<StockSearchInfo>(responseBody);
+                //    searchResults = JsonConvert.DeserializeObject<StockSearchInfo>(responseBody);
 
-                    //Console.WriteLine(searchResults.BestMatches[0].Symbol);
+                //    //Console.WriteLine(searchResults.BestMatches[0].Symbol);
 
 
-                }
+                //}
 
             }
             else
